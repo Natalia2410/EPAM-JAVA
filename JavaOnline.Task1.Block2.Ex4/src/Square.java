4c4f05df1c0ae7de26d7c138b3f667c774bcb5f5
Natalia <50213084+Natalia2410@users.noreply.github.com>
2019-06-07 22:58:46 +0300

    Add files via upload

---
+++ JavaOnline.Task1.Block2.Ex4/src/Square.java
@@ -0,0 +1,17 @@
+public class Square {
+    private int a;
+    private int b;
+
+    public Square(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int getA() {
+        return a;
+    }
+}
