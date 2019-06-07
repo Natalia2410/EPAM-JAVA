3f66ed38c81570884a5c170f6c221a86d131b80c
Natalia <50213084+Natalia2410@users.noreply.github.com>
2019-06-07 23:02:25 +0300

    Add files via upload

---
+++ JavaOnline.Task1.Block3.Ex3/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int max = 100;
+        for (int i = 0; i <= max; i++) {
+            sum += i * i;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
