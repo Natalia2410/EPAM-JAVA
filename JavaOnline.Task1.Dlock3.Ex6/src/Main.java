f57164e47b1159dd5ba71ea78a5bfe5691c5b8a4
Natalia <50213084+Natalia2410@users.noreply.github.com>
2019-06-07 23:04:23 +0300

    Add files via upload

---
+++ JavaOnline.Task1.Dlock3.Ex6/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < Character.MAX_VALUE; i++) {
+            System.out.println(i + " " + (char)i);
+        }
+    }
+}
