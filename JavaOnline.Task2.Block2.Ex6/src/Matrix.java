030634b9bb39c9c16be07a3107c2b2f779f89df3
Natalia <50213084+Natalia2410@users.noreply.github.com>
2019-06-10 18:05:40 +0300

    Add files via upload

---
+++ JavaOnline.Task2.Block2.Ex6/src/Matrix.java
@@ -0,0 +1,36 @@
+public class Matrix {
+    int n;
+    int[][] matrix;
+
+    public void initialize(int[][] array) {
+        this.matrix = array;
+        n = matrix.length;
+        int num = 1;
+        int m = n / 2;
+        if (n % 2 != 0) {
+            System.out.println("n must be even");
+        } else {
+            for (int i = 0; i < m; i++) {
+                for (int j = i; j < n - i; j++) {
+                    matrix[i][j] = num;
+                }
+            }
+            int k = 0;
+            for (int i = n - 1; i >= m ; i--) {
+                for (int j = k; j < n - k; j++) {
+                    matrix[i][j] = num;
+                }
+                k++;
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("| " + matrix[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+    }
+}
