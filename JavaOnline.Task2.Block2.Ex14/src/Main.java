838e1bd7f5388dbf3da25d16482e190601b96251
Natalia <50213084+Natalia2410@users.noreply.github.com>
2019-06-10 23:56:52 +0300

    Add files via upload

---
+++ JavaOnline.Task2.Block2.Ex14/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger(Main.class.getName());
+        FileHandler fh = new FileHandler("log",true);
+        logger.addHandler(fh);
+        Matrix matrix = new Matrix();
+        try {
+            matrix.initialize(new int[4][9]);
+            System.out.println("Original matrix:");
+            matrix.print();
+        } catch (MyException e) {
+            logger.log(Level.INFO, e.getMessage());
+        }
+    }
+}
