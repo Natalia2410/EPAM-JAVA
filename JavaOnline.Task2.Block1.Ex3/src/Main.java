1df5bb85555cef8ab070f2976fc081ec9e135915
Natalia <50213084+Natalia2410@users.noreply.github.com>
2019-06-10 16:36:39 +0300

    Add files via upload

---
+++ JavaOnline.Task2.Block1.Ex3/src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 50;
+        double[] array = new double[n];
+        initialize(array, 100);
+        print(array);
+        System.out.println("count of negative elements: " + getCountOfNegative(array));
+        System.out.println("count of positive elements: " + getCountOfPositive(array));
+        System.out.println("count of null elements: " + getCountOfNull(array));
+
+    }
+
+    public static int getCountOfNegative(double[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int getCountOfPositive(double[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int getCountOfNull(double[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void initialize(double[] array, int range) {
+        Random random = new Random();
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            if (random.nextDouble() > 0.5) {
+                array[i] = random.nextDouble() * range - range / 2;
+            } else {
+                array[i] = 0;
+            }
+        }
+    }
+
+    public static void print(double[] array) {
+        for (double x : array) {
+            System.out.println(x + "");
+        }
+    }
+}
