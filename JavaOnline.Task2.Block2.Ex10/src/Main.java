1fc8a8aec6a17c85b48662c9dac331299c3f98d1
Natalia <50213084+Natalia2410@users.noreply.github.com>
2019-06-10 19:34:08 +0300

    Add files via upload

---
+++ JavaOnline.Task2.Block2.Ex10/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 5;
+        Matrix matrix = new Matrix();
+        matrix.initialize(new int[n][n], 100);
+        System.out.println("Original matrix:");
+        matrix.print();
+        System.out.println("");
+        matrix.positiveElement();
+    }
+}
---
+++ JavaOnline.Task2.Block2.Ex10/src/Matrix.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class Matrix {
+    int n;
+    int[][] matrix;
+
+    public void initialize(int[][] array, int range) {
+        this.matrix = array;
+        n = matrix.length;
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = random.nextInt(range) - range / 2;
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("| " + matrix[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+    }
+
+    public void positiveElement() {
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][i] > 0) {
+                System.out.print(matrix[i][i] + " ");
+            }
+        }
+    }
+}
