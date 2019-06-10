4a53650efaac74dfd5ca72f633f32d91c0f1903b
Natalia <50213084+Natalia2410@users.noreply.github.com>
2019-06-10 18:19:45 +0300

    Add files via upload

---
+++ JavaOnline.Task2.Block2.Ex7/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 5;
+        Matrix matrix = new Matrix();
+        System.out.println("count of positive elements " + matrix.initialize(new double[n][n]));
+        System.out.println("Original matrix:");
+        matrix.print();
+    }
+}
