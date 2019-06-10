c6c2140905d8e31d9f8530d154079a4a10dee1de
Natalia <50213084+Natalia2410@users.noreply.github.com>
2019-06-10 17:45:49 +0300

    Add files via upload

---
+++ JavaOnline.Task2.Block2.Ex5/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int n = 8;
+        Matrix matrix = new Matrix();
+        matrix.initialize(new int[n][n]);
+        if (n % 2 == 0) {
+            System.out.println("Original matrix:");
+            matrix.print();
+        }    }
+}
