d1d6f0988ffe4493363aba27606642cc5071a99e
natalia1986 <naty2410g@gmail.com>
2019-06-19 00:46:22 +0300

    2321: remove all files

--- JavaOnline.Task1.Dlock3.Ex6/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        for (int i = 0; i < Character.MAX_VALUE; i++) {
-            System.out.println(i + " " + (char)i);
-        }
-    }
-}
