8bc7b633ed4b11378138de1b3e40f9ad0cedbaaa
natalia1986 <naty2410g@gmail.com>
2019-06-20 15:00:16 +0300

    add Task3

--- JavaOnlineEpam/src/StringsAndBasicsOfTextProcessing/Block2/Task3/Main.java
+++ JavaOnlineEpam/src/StringsAndBasicsOfTextProcessing/Block2/Task3/Main.java
@@ -2,6 +2,16 @@ package StringsAndBasicsOfTextProcessing.Block2.Task3;
 
 public class Main {
     public static void main(String[] args) {
-        
+        String str = "Hello word drow olleh";
+        String reverseStr = "";
+        char[] symbols = str.toCharArray();
+        for (int i = symbols.length - 1; i >= 0; i--) {
+            reverseStr += symbols[i];
+        }
+        if (str.equalsIgnoreCase(reverseStr)) {
+            System.out.println("word is palindrome");
+        } else {
+            System.out.println("word is not palindrome");
+        }
     }
 }
