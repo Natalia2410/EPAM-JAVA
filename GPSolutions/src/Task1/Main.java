21c105d1972910a9e790ad2059c97b56d7dd63f9
natalia1986 <naty2410g@gmail.com>
2019-07-15 17:36:36 +0300

    no message

--- GPSolutions/src/Task1/Main.java
+++ GPSolutions/src/Task1/Main.java
@@ -13,30 +13,34 @@ public class Main {
         String[] words = null;
         String[] wordsInner = null;
         while ((line = reader.readLine()) != null) {
-            fw.write("     <touristGroup>\n");
-            words = line.split("\\|");
-            for (int i = 0; i < words.length - 1; i++) {
-                wordsInner = words[i].split("\\/");
-                Pattern pattern = Pattern.compile("\\d+");
-                Matcher matcher = pattern.matcher(wordsInner[0]);
-                if (matcher.find()) {
-                    String num = matcher.group();
-                    int touristcType = Integer.parseInt(num);
-                    if (touristcType == 10) {
-                        fw.write("          <tourist type='ADT'>\n");
-                    } else if (touristcType == 8) {
-                        fw.write("          <tourist type='CHD'>" + " " + "birthDate='" + wordsInner[1] + "'>\n");
-                    } else {
-                        fw.write("          <tourist type='INF'>" + " " + "birthDate='" + wordsInner[1] + "'>\n");
+            if (!line.matches("<tourists/>")) {
+                fw.write("     <touristGroup>\n");
+                words = line.split("\\|");
+                for (int i = 0; i < words.length - 1; i++) {
+                    wordsInner = words[i].split("\\/");
+                    Pattern pattern = Pattern.compile("\\d+");
+                    Matcher matcher = pattern.matcher(wordsInner[0]);
+                    if (matcher.find()) {
+                        String num = matcher.group();
+                        int touristcType = Integer.parseInt(num);
+                        if (touristcType == 10) {
+                            fw.write("          <tourist type='ADT'>\n");
+                        } else if (touristcType == 8) {
+                            fw.write("          <tourist type='CHD'>" + " " + "birthDate='" + wordsInner[1] + "'>\n");
+                        } else {
+                            fw.write("          <tourist type='INF'>" + " " + "birthDate='" + wordsInner[1] + "'>\n");
+                        }
                     }
+                    fw.write("               <prefix>" + wordsInner[4] + "/<prefix>\n");
+                    fw.write("               <name>" + wordsInner[3] + "/<name>\n");
+                    fw.write("               <surname>" + wordsInner[2] + "/<surname>\n");
+                    fw.write("          </tourist>\n");
                 }
-                fw.write("               <prefix>" + wordsInner[4] + "/<prefix>\n" );
-                fw.write("               <name>" + wordsInner[3] + "/<name>\n");
-                fw.write("               <surname>" + wordsInner[2] + "/<surname>\n");
-                fw.write("          </tourist>\n");
+                fw.write("     </touristGroup>\n");
             }
+
         }
-        fw.write("     </touristGroup>\n");
+
         fw.write("</touristInfo>\n");
         fw.close();
     }
